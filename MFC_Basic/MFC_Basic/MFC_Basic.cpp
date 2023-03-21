3f96f4e2aee15d327871d1ceaf62de50d0eda3cd
ANTL-KJH <wogus9190@gmail.com>
2023-03-21 20:05:14 +0900

    20203 DataCommunication

---
+++ MFC_Basic/MFC_Basic/MFC_Basic.cpp
@@ -0,0 +1,58 @@
+﻿/*
+Project : MFC_BASIC
+Author : JH KIM
+First Write Date : 2023.03.21
+*/
+
+// MFC_Basic.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFC_Basic.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 유일한 애플리케이션 개체입니다.
+
+CWinApp theApp;
+
+using namespace std;
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        // MFC를 초기화합니다. 초기화하지 못한 경우 오류를 인쇄합니다.
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: 여기에 애플리케이션 동작을 코딩합니다.
+            wprintf(L"심각한 오류: MFC 초기화 실패\n");
+            nRetCode = 1;
+        }
+        else
+        {
+            // TODO: 여기에 애플리케이션 동작을 코딩합니다.
+            CString str;
+            //str.LoadString(IDS_APP_TITLE);
+            str.LoadString(IDS_TEST);       // 실행은 되나 빨간 줄 발생, string이 한국어인경우 ?출력
+            _tsetlocale(LC_ALL, _T(""));    // 한글 출력을 위한 설정
+            _tprintf(_T("Hello from %s!\n"), str);
+        }
+    }
+    else
+    {
+        // TODO: 오류 코드를 필요에 따라 수정합니다.
+        wprintf(L"심각한 오류: GetModuleHandle 실패\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
